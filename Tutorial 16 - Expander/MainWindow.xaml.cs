09fbeb8302360a9d9be7405855696705f4969570
Phil Henning <philiphenning@outlook.com>
2024-10-16 10:32:27 +0100

    Completed tutorial 15

---
+++ Tutorial 16 - Expander/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Tutorial_16___Expander
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
